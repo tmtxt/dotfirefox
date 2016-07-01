3b2aeee266008274e8f7ab451a9610f06232c84f
Truong <me@truongtx.me>
2016-07-01 21:35:45 +0700

    use firefox javascript module to load ~/dff instead of commonjs

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const {env} = require('sdk/system/environment');
-var {Ci, Cc} = require('chrome');
-var {Services}  = require('resource://gre/modules/Services.jsm');
+var {Ci, Cc, Cu} = require('chrome');
 
-// dotfirefox path
-const homePath = env.HOME;
-const dotfirefoxPath = `${homePath}/dotfirefox/`;
+var dff = Cu.import('file:///Volumes/tmtxt/dff/index.js', {});
+var { Class } = require('sdk/core/heritage');
+var { Unknown, Factory } = require('sdk/platform/xpcom');
 
-// register 'dotfirefox' as an alias path, taken from
-// https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/Using
-var resProt = Services.io.getProtocolHandler('resource')
-      .QueryInterface(Ci.nsIResProtocolHandler);
-var aliasFile = Cc['@mozilla.org/file/local;1']
-      .createInstance(Ci.nsILocalFile);
-aliasFile.initWithPath(dotfirefoxPath);
-var aliasURI = Services.io.newFileURI(aliasFile);
-resProt.setSubstitution('dotfirefox', aliasURI);
-
-// create custom loader for dotfirefox dir and load ~/dotfirefox as main application
-// https://developer.mozilla.org/en-US/Add-ons/SDK/Low-Level_APIs/_loader
-let {main, Loader} = require('toolkit/loader');
-let options = require('@loader/options');
-options.paths['dotfirefox/'] = 'resource://dotfirefox/';
-let loader = Loader(options);
-main(loader, 'dotfirefox/index.js');
+var contractId = '@truongtx.me/dotfirefox';
+var DotFirefox = Class({
+  extends: Unknown,
+  get wrappedJSObject() {
+    return dff;
+  }
+});
+Factory({
+  contract: contractId,
+  Component: DotFirefox
+});
