d6788f69c5899dd5f959f4fd1a67f44727a4ec65
Truong <me@truongtx.me>
2016-07-01 21:45:48 +0700

    auto detect home path

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-var {Ci, Cc, Cu} = require('chrome');
-
-var dff = Cu.import('file:///Volumes/tmtxt/dff/index.js', {});
+// libs
+var { env } = require('sdk/system/environment');
+var { Cu } = require('chrome');
 var { Class } = require('sdk/core/heritage');
 var { Unknown, Factory } = require('sdk/platform/xpcom');
 
+// dotfirefox config file path
+var homePath = env.HOME;
+var dotfirefoxPath = `${homePath}/dotfirefox`;
+
+// load dotfirefox config file
+var dotfirefox = Cu.import(`file://${dotfirefoxPath}/index.js`, {});
+
+// register it as a Cc
 var contractId = '@truongtx.me/dotfirefox';
 var DotFirefox = Class({
   extends: Unknown,
   get wrappedJSObject() {
-    return dff;
+    return dotfirefox;
   }
 });
 Factory({
